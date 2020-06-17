a78ce29003de6c06ca4c7cb1b847b02d7a14c65a
Satish Kumar Anbalagan <46766786+SatishKumarAnbalagan@users.noreply.github.com>
2020-06-17 18:14:14 -0400

    pre process HED

---
+++ preprocessHED.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+
+import torch
+
+import getopt
+import math
+import numpy
+import os
+import PIL
+import PIL.Image
+import sys
+import glob
+
+##########################################################
+
+assert(int(str('').join(torch.__version__.split('.')[0:2])) >= 13) # requires at least pytorch version 1.3.0
+
+torch.set_grad_enabled(False) # make sure to not compute gradients for computational performance
+
+torch.backends.cudnn.enabled = True # make sure to use cudnn for computational performance
+
+##########################################################
+
+arguments_strModel = 'bsds500'
+arguments_strIn = './input'
+arguments_strOut = './output'
+
+for strOption, strArgument in getopt.getopt(sys.argv[1:], '', [ strParameter[2:] + '=' for strParameter in sys.argv[1::2] ])[0]:
+	if strOption == '--model' and strArgument != '': arguments_strModel = strArgument # which model to use
+	if strOption == '--in' and strArgument != '': arguments_strIn = strArgument # path to the input image
+	if strOption == '--out' and strArgument != '': arguments_strOut = strArgument # path to where the output should be stored
+# end
+
+##########################################################
+
+class Network(torch.nn.Module):
+	def __init__(self):
+		super(Network, self).__init__()
+
+		self.netVggOne = torch.nn.Sequential(
+			torch.nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False)
+		)
+
+		self.netVggTwo = torch.nn.Sequential(
+			torch.nn.MaxPool2d(kernel_size=2, stride=2),
+			torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False)
+		)
+
+		self.netVggThr = torch.nn.Sequential(
+			torch.nn.MaxPool2d(kernel_size=2, stride=2),
+			torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False)
+		)
+
+		self.netVggFou = torch.nn.Sequential(
+			torch.nn.MaxPool2d(kernel_size=2, stride=2),
+			torch.nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False)
+		)
+
+		self.netVggFiv = torch.nn.Sequential(
+			torch.nn.MaxPool2d(kernel_size=2, stride=2),
+			torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False),
+			torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+			torch.nn.ReLU(inplace=False)
+		)
+
+		self.netScoreOne = torch.nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0)
+		self.netScoreTwo = torch.nn.Conv2d(in_channels=128, out_channels=1, kernel_size=1, stride=1, padding=0)
+		self.netScoreThr = torch.nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1, stride=1, padding=0)
+		self.netScoreFou = torch.nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
+		self.netScoreFiv = torch.nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
+
+		self.netCombine = torch.nn.Sequential(
+			torch.nn.Conv2d(in_channels=5, out_channels=1, kernel_size=1, stride=1, padding=0),
+			torch.nn.Sigmoid()
+		)
+
+		self.load_state_dict({ strKey.replace('module', 'net'): tenWeight for strKey, tenWeight in torch.load(__file__.replace('preprocessHED.py', 'network-' + arguments_strModel + '.pytorch')).items() })
+	# end
+
+	def forward(self, tenInput):
+		tenBlue = (tenInput[:, 0:1, :, :] * 255.0) - 104.00698793
+		tenGreen = (tenInput[:, 1:2, :, :] * 255.0) - 116.66876762
+		tenRed = (tenInput[:, 2:3, :, :] * 255.0) - 122.67891434
+
+		tenInput = torch.cat([ tenBlue, tenGreen, tenRed ], 1)
+
+		tenVggOne = self.netVggOne(tenInput)
+		tenVggTwo = self.netVggTwo(tenVggOne)
+		tenVggThr = self.netVggThr(tenVggTwo)
+		tenVggFou = self.netVggFou(tenVggThr)
+		tenVggFiv = self.netVggFiv(tenVggFou)
+
+		tenScoreOne = self.netScoreOne(tenVggOne)
+		tenScoreTwo = self.netScoreTwo(tenVggTwo)
+		tenScoreThr = self.netScoreThr(tenVggThr)
+		tenScoreFou = self.netScoreFou(tenVggFou)
+		tenScoreFiv = self.netScoreFiv(tenVggFiv)
+
+		tenScoreOne = torch.nn.functional.interpolate(input=tenScoreOne, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+		tenScoreTwo = torch.nn.functional.interpolate(input=tenScoreTwo, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+		tenScoreThr = torch.nn.functional.interpolate(input=tenScoreThr, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+		tenScoreFou = torch.nn.functional.interpolate(input=tenScoreFou, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+		tenScoreFiv = torch.nn.functional.interpolate(input=tenScoreFiv, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+
+		return self.netCombine(torch.cat([ tenScoreOne, tenScoreTwo, tenScoreThr, tenScoreFou, tenScoreFiv ], 1))
+	# end
+# end
+
+netNetwork = None
+
+##########################################################
+
+def estimate(tenInput):
+	global netNetwork
+
+	if netNetwork is None:
+		netNetwork = Network().cuda().eval()
+	# end
+
+	intWidth = tenInput.shape[2]
+	intHeight = tenInput.shape[1]
+
+	#assert(intWidth == 480) # remember that there is no guarantee for correctness, comment this line out if you acknowledge this and want to continue
+	#assert(intHeight == 320) # remember that there is no guarantee for correctness, comment this line out if you acknowledge this and want to continue
+
+	return netNetwork(tenInput.cuda().view(1, 3, intHeight, intWidth))[0, :, :, :].cpu()
+# end
+
+##########################################################
+
+if __name__ == '__main__':
+	"""
+	for root, dirs, files in os.walk(arguments_strIn):
+		for file in files:
+			if file.endswith(".png"):
+				imageFile = os.path.join(root, file)
+		#for imageFile in glob.glob(arguments_strIn+'/*'):
+				print(imageFile)
+				outputImageFile = arguments_strOut + "/" +"".join(imageFile.split('/')[-1:]).split('.')[0] + "_out." + str(imageFile.split('.')[-1:][0])
+				print(outputImageFile)
+				with PIL.Image.open(imageFile) as inImage:
+					cover = inImage.convert('RGB')
+					cover = cover.resize((480, 320))
+					cover.save(imageFile, inImage.format)
+
+					tenInput = torch.FloatTensor(numpy.array(PIL.Image.open(imageFile))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0))
+
+					tenOutput = estimate(tenInput)
+
+					PIL.Image.fromarray((tenOutput.clamp(0.0, 1.0).numpy().transpose(1, 2, 0)[:, :, 0] * 255.0).astype(numpy.uint8)).save(outputImageFile)
+	"""
+	
+	inputs = os.listdir(arguments_strIn)
+	i = 0
+	for input in inputs:
+		print(i)
+		i+=1
+		tenInput = torch.FloatTensor(numpy.array(PIL.Image.open(arguments_strIn + '/' + input))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0))
+
+		tenOutput = estimate(tenInput[:3])
+
+		PIL.Image.fromarray((tenOutput.clamp(0.0, 1.0).numpy().transpose(1, 2, 0)[:, :, 0] * 255.0).astype(numpy.uint8)).save(arguments_strOut + '/' + input)
+# end
